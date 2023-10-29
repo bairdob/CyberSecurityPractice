370c0a45a9b349022e1e02180e6beea0df3fc701
bairdob <krumane@mail.ru>
2023-10-29 16:40:23 +0300

    add database integration and new API endpoints

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from src.database import SessionLocal
+from src.models import User, UserResponse, Role, RoleResponse
 
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/ping")
 async def ping():
     return 'pong'
+
+
+# Get all users
+@app.get("/users/", response_model=list[UserResponse])
+def get_all_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    users = db.query(User).offset(skip).limit(limit).all()
+    return users
+
+
+@app.get("/roles", response_model=list[RoleResponse])
+def get_all_roles(db: Session = Depends(get_db)):
+    roles = db.query(Role).all()
+    return roles

c268d29c0ac79e0049950a67b331811bf4e856d1
bairdob <krumane@mail.ru>
2023-11-11 20:45:27 +0300

    Reordered authentication checks

--- src/auth/router.py
+++ src/auth/router.py
@@ -23,15 +23,15 @@ async def read_users_me(current_user: Annotated[User, Depends(get_current_user)]
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
     user = verify_user(form_data.username, db)
 
-    totp_code = pyotp.TOTP(user.totp_secret).now()
-    if totp_code != form_data.client_secret:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
-
     if not user:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     hashed_password = users.calc_hash_password(form_data.password, user)
-    if not hashed_password == users.get_hashed_password(db, user):
+    if hashed_password != users.get_hashed_password(db, user):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    totp_code = pyotp.TOTP(user.totp_secret).now()
+    if totp_code != form_data.client_secret:
         raise HTTPException(status_code=401, detail="Invalid credentials")
 
     access_token = user.username
